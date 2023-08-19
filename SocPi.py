c97aa3be0524b834499dfe8940066484cae799bc
Rymazon <Ryanscomazzon@gmail.com>
2023-08-19 01:19:20 -0400

    mok

--- SocPi.py
+++ SocPi.py
@@ -3,6 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
+print("Socket initialized")
 
 while True:
     clientsocket, address = s.accept()
