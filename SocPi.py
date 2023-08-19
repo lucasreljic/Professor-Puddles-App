966bb9fdfc84c331a066359e767562091b39d956
Rymazon <Ryanscomazzon@gmail.com>
2023-08-19 04:43:38 -0400

    munkui

--- SocPi.py
+++ SocPi.py
@@ -5,14 +5,14 @@ import socket
 port = 8833
 host = '192.168.137.212' 
 
+def Action0():
+    print("0")
+
 def Action1():
     print("1")
 
 def Action2():
     print("2")
-
-def Action3():
-    print("3")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -26,12 +26,12 @@ while True:
 
     v = clientsocket.recv(8).decode('utf-8')
     while v != "end":
+        if v == "0":
+            Action0()
         if v == "1":
             Action1()
         if v == "2":
             Action2()
-        if v == "3":
-            Action3()
         v = clientsocket.recv(8).decode('utf-8')
 
     print("Client disconnected")
