912e79142b1fc3bf96b0584aa6a51e0999328c9b
Rymazon <Ryanscomazzon@gmail.com>
2023-08-19 18:16:42 -0400

    m

--- Servo.py
+++ Servo.py
@@ -8,10 +8,11 @@ GPIO.setmode(GPIO.BCM)
 minPW=0.001
 maxPW=0.002
 
+servo = AngularServo(18,min_pulse_width=minPW,max_pulse_width=maxPW)
+
 try:
-    servo = AngularServo(18,min_pulse_width=minPW,max_pulse_width=maxPW)
-    servo.vaue = 0.5
-    
+    servo.value = 0.5
+    sleep 1
 
 finally:
     GPIO.cleanup()
