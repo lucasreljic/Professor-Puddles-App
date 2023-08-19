079aa9df17e4ad16bd450f15ef94432563eb5365
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-19 15:48:17 -0400

    make UI for the page

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.ttk import *
+from tkinter import Canvas, OptionMenu
 import cv2
 import json
 from main import main
@@ -13,63 +13,45 @@ newToast = Toast()
 
 class GUI:
 
-    def __init__(self, root, video_source=0, img=None):
+    def __init__(self, root, video_source=0):
         self.root = root
         self.root.title("Posture Corrector")
-        self.root.geometry("800x400")
-        self.i = 0
-        style = Style()
-        style.theme_use("clam")
+        self.root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-        # Configure the style for the various widgets
-        style.configure("TButton",
-                        background="black",
-                        foreground="white",
-                        padding=10,
-                        font=("Helvetica", 12, "bold"))
-
-        style.configure("TLabel",
-                        foreground="#333",
-                        font=("Helvetica", 14))
-
-        style.configure("TEntry",
-                        fieldbackground="white",
-                        font=("Helvetica", 12))
-
-        style.configure("TMenubutton",
-                        background="white",
-                        font=("Helvetica", 12))
         self.video_source = video_source
         self.vid = cv2.VideoCapture(self.video_source)
         self.detector = main()
-        width, height = 1080, 1920
-        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 2020)
 
-        self.label_widget = tk.Label(root)
+        self.label_widget = tk.Label(root, bg="white", borderwidth=2, relief="solid")
+        self.label_widget.place(relx=0.6, rely=0.1, relwidth=0.3, relheight=0.7)
 
-        self.btn_start = Button(root, text="Start", width=10, command=self.start)
-        self.btn_start.pack(padx=10, pady=5)
+        self.btn_start = self.create_rounded_button("Start", "light green", self.start, 0.05, 0.1)
+        self.btn_stop = self.create_rounded_button("Stop", "#FF8888", self.stop, 0.05, 0.3)
+        self.btn_setup = self.create_rounded_button("Setup", "light blue", self.setup, 0.05, 0.5)
 
-        self.btn_stop = Button(root, text="Stop", width=10, command=self.stop)
-        self.btn_stop.pack(padx=10, pady=5)
-
-        self.btn_start = Button(root, text="Setup", width=10, command=self.setup)
-        self.btn_start.pack(padx=10, pady=5)
         dropdown_var = tk.StringVar()
         dropdown_var.set("Configs")
 
-        # Read data from the JSON file
         with open('data.json') as json_file:
             loaded_data = json.load(json_file)["people"]
-        dropdown = [loaded_data[0]["name"], loaded_data[1]["name"], loaded_data[2]["name"], loaded_data[3]["name"]]
+        dropdown = [loaded_data[i]["name"] for i in range(4)]
 
-        dropdown = OptionMenu(root, dropdown_var, *dropdown)
-        dropdown.pack()
-        self.label_widget.pack()
+        dropdown_menu = OptionMenu(root, dropdown_var, *dropdown)
+        dropdown_menu.place(relx=0.05, rely=0.7)
 
         self.is_playing = False
         self.update()
+
+    def create_rounded_button(self, text, color, cmd, relx, rely):
+        canvas = Canvas(self.root, bg='white', bd=0, highlightthickness=0, relief='ridge')
+        canvas.place(relx=relx, rely=rely, relwidth=0.15, relheight=0.1)
+        canvas.create_rectangle(10, 10, 10 + 150, 10 + 40, outline=color, fill=color, width=2)
+        btn_id = canvas.create_text(85, 30, text=text, fill='white', font=("Helvetica", 12, "bold"))
+        canvas.tag_bind(btn_id, '<ButtonPress-1>', lambda event, c=cmd: c())
+
+        return canvas
 
     def start(self):
         if (not self.is_playing):
@@ -91,7 +73,6 @@ class GUI:
         # Capture the video frame by frame
 
         _, img = self.vid.read()
-        self.i += 1
 
         # Setup
         img = self.detector.find_pose(img)
@@ -117,31 +98,19 @@ class GUI:
             good_poster = False
         print(good_poster)
 
-        if not good_poster and self.i > 100:
+        if not good_poster:
             newToast.text_fields = ['!']
             newToast.on_activated = lambda _: print('Toast clicked!')
             toaster.show_toast(newToast)
-            self.i = 0
         # self.detector.showFps(frame)
         opencv_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
 
         if self.is_playing:
-            # Convert image from one color space to other
             opencv_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-
-            # Capture the latest img and transform to image
             captured_image = Image.fromarray(opencv_image)
-
-            # Convert captured image to photoimage
             photo_image = ImageTk.PhotoImage(image=captured_image)
-
-            # Displaying photoimage in the label
             self.label_widget.photo_image = photo_image
-
-            # Configure image in the label
             self.label_widget.configure(image=photo_image)
-
-            # Repeat the same process after every 10 seconds
             self.label_widget.after(10, self.update)
 
     def __del__(self):
@@ -151,9 +120,9 @@ class GUI:
 
 def gui():
     root = tk.Tk()
-    root.bind('<Escape>', lambda e: app.quit())
+    root.configure(bg="white")
+    root.bind('<Escape>', lambda e: root.quit())
     app = GUI(root)
-    # app.update(img)
 
     root.mainloop()
 
