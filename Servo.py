8e686bd473eb0e62c1cf24b8f8b057d079950d74
Rymazon <Ryanscomazzon@gmail.com>
2023-08-19 18:30:04 -0400

    m

--- Servo.py
+++ Servo.py
@@ -11,8 +11,14 @@ maxPW=0.002
 servo = AngularServo(18,min_pulse_width=minPW,max_pulse_width=maxPW)
 
 try:
-    servo.value = 0.5
-    sleep 1
+    servo.value = -0.5
+    sleep(2)
+
+    for i in range(5)
+        servo.value = -1
+        sleep(0.333)
+        servo.value = -0.5
+        sleep(0.333)
 
 finally:
     GPIO.cleanup()
