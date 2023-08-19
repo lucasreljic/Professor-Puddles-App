d1f9af8a37f2499b76e610c752d9d39d8a012884
lucasreljic <lucas.reljic@gmail.com>
2023-08-19 18:58:31 -0400

    working json

--- src/gui.py
+++ src/gui.py
@@ -2,12 +2,8 @@ import tkinter as tk
 from tkinter.ttk import *
 import cv2
 import json
-from main import main
+from main import main, run  
 from PIL import Image, ImageTk
-from windows_toasts import Toast, WindowsToaster
-toaster = WindowsToaster('Python')
-newToast = Toast()
-
 class GUI:
     def __init__(self, root, video_source=0, img = None):
         self.root = root
@@ -52,15 +48,17 @@ class GUI:
         
         self.btn_start = Button(root, text="Setup" ,width=10, command=self.setup)
         self.btn_start.pack(padx=10, pady=5)
-        dropdown_var = tk.StringVar()
-        dropdown_var.set( "Configs" )
         # Read data from the JSON file
+        self.firstRun = True
+        self.integer = 0
         with open('data.json') as json_file:
-            loaded_data = json.load(json_file)["people"]
-        dropdown = [loaded_data[0]["name"], loaded_data[1]["name"], loaded_data[2]["name"], loaded_data[3]["name"]]
+            self.loaded_data = json.load(json_file)
+        dropdown = [self.loaded_data[0]["name"],self.loaded_data[0]["name"], self.loaded_data[1]["name"], self.loaded_data[2]["name"], self.loaded_data[3]["name"]]
         
-        
-        dropdown = OptionMenu(root, dropdown_var, *dropdown)
+        self.dropdown_var = tk.StringVar()
+        self.dropdown_var.set(self.loaded_data[0]["name"])
+        dropdown = OptionMenu(root, self.dropdown_var, *dropdown)
+        print(self.dropdown_var.get())
         dropdown.pack()
         self.label_widget.pack()
         
@@ -70,12 +68,14 @@ class GUI:
     def start(self):
         if(not self.is_playing):
             self.is_playing = True
+            self.firstRun = True
             self.update()
 
     def stop(self):
         self.is_playing = False
 
     def setup(self):
+        self.firstRun = True
         entry1 = tk.Entry(self.root) 
         # canvas1.create_window(200, 140, window=entry1)
         # data
@@ -84,42 +84,17 @@ class GUI:
         self.setup = False
     def update(self):
         # Capture the video frame by frame
-
+        if self.firstRun:
+            for index, record in enumerate(self.loaded_data):
+                if record["name"] == str(self.dropdown_var.get()):
+                    self.integer = index
+                    self.firstRun = False
         _, img = self.vid.read()
-        self.i += 1
-
-        # Setup
-        img = self.detector.find_pose(img)
-        self.detector.get_position(img)  # DO NOT DELETE: this will give the landmark list
-
-        # Interested angle
-        # r_turn = self.detector.find_angle(img, 6, 8, 0)
-        # l_turn = self.detector.find_angle(img, 3, 7, 0)
-        front_posture = self.detector.find_angle(img, 11, 0, 12)
-        left_shoulder = self.detector.find_angle(img, 9, 11, 12)
-        right_shoulder = self.detector.find_angle(img, 10, 12, 11)
-
-        good_poster = True
-
-        # TODO: add an interator for good_posture so it only sends a notification if you slouch
-        #  for a certain amount of time
-
-        # TODO: right now the following measurements are for me. we need code to make it personalized
-        if front_posture < 75 \
-                or front_posture > 95 \
-                or left_shoulder < 310 or left_shoulder > 320 \
-                or right_shoulder < 40 or right_shoulder > 50:
-            good_poster = False
-        print(good_poster)
-
-        if not good_poster and self.i > 100:
-            newToast.text_fields = ['!']
-            newToast.on_activated = lambda _: print('Toast clicked!')
-            toaster.show_toast(newToast)
-            self.i = 0
-        # self.detector.showFps(frame)
+        img = run(img, self.i, self.detector, self.loaded_data, self.integer)
         opencv_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
 
+
+        # needs to be here cannot be in backend
         if self.is_playing:
             # Convert image from one color space to other
             opencv_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
