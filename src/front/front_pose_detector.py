5b41eebe63053604f09d0bb2bd742e6fdf796f3e
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-20 01:16:07 -0400

    fix typo

--- src/front/front_pose_detector.py
+++ src/front/front_pose_detector.py
@@ -56,7 +56,7 @@ class FrontPoseDetector():
 
         # Calculate the angle
         angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
-        # some time this angle comes zero, so below conditon we added
+        # some time this angle comes zero, so below condition we added
         if angle < 0:
             angle += 360
 
